d4d7b1cad1e68f2cebd52cd7ee99360585545609
Felipebatistapastore <125515160+Felipebatistapastore@users.noreply.github.com>
2023-02-17 21:19:36 -0300

    Add files via upload

---
+++ GUSTAVO/PHP/2_variaveis.php
@@ -0,0 +1,9 @@
+<?php
+    $name = "Guilherme";
+	$age = 24;
+	
+	echo $name;
+	echo "<br>";
+	echo $age; 
+	
+?>
---
+++ GUSTAVO/PHP/php.php
@@ -0,0 +1,11 @@
+<?php
+  echo "isso é um comentário";
+  // comentário 1
+  
+  /* comentário 2
+      em duas linhas
+	  
+	  */
+	  
+	  # comentário 3
+	  ?>
